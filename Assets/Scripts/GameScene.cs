2d75b886b0f009427bcbb8c5f33b4798fd3da226
Michael Mailloux <mmailloux22@gmail.com>
2020-08-05 21:50:01 -0400

    Added paddle movement
    Created paddle objects, gamescene script, and paddle movement method

---
+++ Assets/Scripts/GameScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScene : MonoBehaviour
+{
+    private Paddle playerOnePaddle;
+    private Paddle playerTwoPaddle;
+    private Rigidbody2D rbPlayerOne;
+    private Rigidbody2D rbPlayerTwo;
+
+    void Awake() {
+        GameObject playerOne = GameObject.Find("PlayerOne");
+        GameObject playerTwo = GameObject.Find("PlayerTwo");
+
+        rbPlayerOne = playerOne.GetComponent<Rigidbody2D>();
+        rbPlayerTwo = playerTwo.GetComponent<Rigidbody2D>();
+        playerOnePaddle = new Paddle("VerticalPlayerOne", rbPlayerOne);
+        playerTwoPaddle = new Paddle("VerticalPlayerTwo", rbPlayerTwo);
+    }
+
+    void FixedUpdate()
+    {
+        playerOnePaddle.move();
+        playerTwoPaddle.move();
+    }
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
-public class Paddle : MonoBehaviour
+public class Paddle
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private string verticalControls;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+    public float speed = 10;
+
+    public Paddle(string controls, Rigidbody2D playerRigidBody) {
+        verticalControls = controls;
+        rb = playerRigidBody;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void move() {
+        Vector2 moveInput = new Vector2(0, Input.GetAxis(verticalControls));
+        moveVelocity = moveInput * speed;
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
 }
