e3982c859f570192b2b19181a9c9332a764ca83b
Michael Mailloux <mmailloux22@gmail.com>
2020-08-06 20:22:26 -0400

    Added wall collision
    Added collision to walls and made pong paddle prefab

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -8,7 +8,7 @@ public class Paddle
     private string verticalControls;
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
-    public float speed = 10;
+    public float speed = 12;
 
     public Paddle(string controls, Rigidbody2D playerRigidBody) {
         verticalControls = controls;
@@ -16,7 +16,7 @@ public class Paddle
     }
 
     public void move() {
-        Vector2 moveInput = new Vector2(0, Input.GetAxis(verticalControls));
+        Vector2 moveInput = new Vector2(0, Input.GetAxisRaw(verticalControls));
         moveVelocity = moveInput * speed;
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
